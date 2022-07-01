c0f39db70f4c4103612c06ce725e50edd8c76690
thesherifff <jamalisah@icloud.com>
2022-07-01 20:46:37 +0000

    Added ability to tally correct/incorrect scores

--- js/script.js
+++ js/script.js
@@ -50,8 +50,10 @@ let isCorrect = userAnswer === calculatedAnswer[0];
 
 if (isCorrect){
     alert("Hey! You got it right! Snyggt! :D ")
+    incrementScore();
 } else{
     alert(`Hoppla Kerstin! You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    incrementWrongAnswer();
 }
 
 runGame(calculatedAnswer[1]);
@@ -78,11 +80,24 @@ if (operator === "+"){
 
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1.
+ */
+
 function incrementScore() {
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+    
 
 }
 
+/**
+ * Gets the current tally of incorrect answers from the dom
+ */
+
 function incrementWrongAnswer() {
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
